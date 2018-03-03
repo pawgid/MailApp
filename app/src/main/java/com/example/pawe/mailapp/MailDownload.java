60b31b32423a5747c34b8946d7b5e0b39db2c20e
pawgid <pgidaszewski@gmail.com>
2018-03-03 16:20:55 +0100

    Typo fix

---
+++ app/src/main/java/com/example/pawe/mailapp/MailDownload.java
@@ -0,0 +1,61 @@
+package com.example.pawe.mailapp;
+
+
+import android.app.Notification;
+import android.content.pm.PackageInstaller;
+import android.util.Log;
+
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+/**
+ * Created by Paweł on 2018-03-03.
+ */
+
+public class MailDownload {
+
+    public static void main(String args[]) throws Exception {
+
+        // mail server connection parameters
+        String host = "pop.gmail.com";
+        String user = "testapp243243@gmail.com";
+        String password = "testapp1";
+
+        // connect to my pop3 inbox
+        Properties properties = System.getProperties();
+        Session session = Session.getDefaultInstance(properties);
+        Store store = session.getStore("pop3");
+        store.connect(host, user, password);
+        Folder inbox = store.getFolder("Inbox");
+        inbox.open(Folder.READ_ONLY);
+
+        // get the list of inbox messages
+        Message[] messages = inbox.getMessages();
+
+        if (messages.length == 0) System.out.println("No messages found.");
+
+        for (int i = 0; i < messages.length; i++) {
+            // stop after listing ten messages
+            if (i > 10) {
+                System.exit(0);
+                inbox.close(true);
+                store.close();
+            }
+
+            System.out.println("Message " + (i + 1));
+            System.out.println("From : " + messages[i].getFrom()[0]);
+            System.out.println("Subject : " + messages[i].getSubject());
+            System.out.println("Sent Date : " + messages[i].getSentDate());
+            System.out.println();
+        }
+
+        inbox.close(true);
+        store.close();
+    }
+}
